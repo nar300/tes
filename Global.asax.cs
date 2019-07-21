bf0033431c653b1d39880095e2b86c3ff59d1378
nar300 <50659130+nar300@users.noreply.github.com>
2019-07-21 12:40:15 +0100

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using MVC_withAutomapper.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MVC_withAutomapper
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        [Obsolete]
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutoMapper.Mapper.Initialize(x => x.AddProfile<AutoMapperProfile>());
+        }
+    }
+}
